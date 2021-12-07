5e4e89b24108aeffeb8d16a8ddc43d6a7bf4a26a
LukaBanjanin <lbanjani@uwo.ca>
2021-12-07 17:10:42 -0500

    adding fflush statements after printf to make compatible with eclipse console

--- phone.c
+++ phone.c
@@ -7,31 +7,43 @@ int main () {
 	int x,y,z,r,c;
 
 	printf("Input 10 digit Phone Number ");
+	fflush(stdout);
+
 	r = scanf("%3d%3d%4d", &x, &y, &z);
+
 	if (r != 3){ 
 	printf("Invalid phone number!\n");
+	fflush(stdout);
+
 	return(1);
 }
 // clear the input buffer
 	while ((getchar()) != '\n');
 	printf("Format Options\n A) Local\n B) Domestic\n C) International\n D) Odd\n\n Input Option: ");
+	fflush(stdout);
+
 	c = getchar();
 //case matching to indicate format of output
 	switch (c) {
 		case 'a':
 		case 'A': printf("\nPhone Number: %d - %d\n", y, z);
+			fflush(stdout);
 			  break;
 		case 'b':
 		case 'B': printf("\nPhone Number: (%d) %d - %d\n", x, y, z);
+			fflush(stdout);
 			  break; 
 		case 'c':
 		case 'C': printf("\nPhone Numebr: +1-%d-%d-%d\n", x, y, z);
+			fflush(stdout);
 			  break;
 		case 'd':
-		case 'D': printf("\n0%d 0%d %d\n", x, y, z);	
+		case 'D': printf("\n0%d 0%d %d\n", x, y, z);
+			fflush(stdout);
 			  break;
 
 		default: printf("\nError invalid option!\n");
+			fflush(stdout);
 			  break;
 	}
 	return (0);
