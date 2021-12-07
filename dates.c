872543edb338fb6f868eedbb959e25e587af03a0
LukaBanjanin <lbanjani@uwo.ca>
2021-12-07 14:14:23 -0500

    fixing bug caused by improper use of OR operator in conditional if statements as opposed to AND operator

--- dates.c
+++ dates.c
@@ -8,30 +8,33 @@ int main(void) {
 // declare all int variables
 	int m,d,y,r,monthMAX = 0, dayMAX = 0, yearMAX = 0;
 	do {
-	printf("Enter a date (mm/dd/yy) (or enter 0/0/0 when you are done entering dates and wish to see the largest date you entered):  ");
+	printf("Enter a date (mm/dd/yyyy) (or enter 0/0/0 when you are done entering dates and wish to see the largest date you entered):  ");
 	r = scanf("%d/%d/%d", &m, &d, &y);
 //start the error checking
 	if (r != 3)
-	printf("Invalid Date Format!\n");
+		printf("Invalid Date Format!\n");
 	else if (m < 0 || m > 12)
-	printf("Invalid Month Number\n");
+		printf("Invalid Month Number\n");
 	else if (d < 0 || d > 31)
-	printf("Invalid Day Number\n");
-	else if (y < 0 || y > 99)
-	printf("Invalid Year Number\n");
-//start comparing the dates
+		printf("Invalid Day Number\n");
+	else if (y < 0 || y > 9999)
+		printf("Invalid Year Number\n");
+	//start comparing the dates
 	else if (y > yearMAX) {
-	yearMAX = y;
-	dayMAX = d;
-	monthMAX = m; }
-	else if (y == yearMAX || m > monthMAX) {
-	yearMAX = y;
-	dayMAX = d;
-	monthMAX = m; }
-	else if (y == yearMAX || m == monthMAX || d > dayMAX) {
-	yearMAX = y;
-	dayMAX = d;
-	monthMAX = m; }
+		yearMAX = y;
+		dayMAX = d;
+		monthMAX = m;
+	}
+	else if (y == yearMAX && m > monthMAX) {
+		yearMAX = y;
+		dayMAX = d;
+		monthMAX = m;
+	}
+	else if (y == yearMAX && m == monthMAX && d > dayMAX) {
+		yearMAX = y;
+		dayMAX = d;
+		monthMAX = m;
+	}
 
 	} while (m != 0 && d != 0 && y != 0);
 	printf("%d/%d/%d is the latest date \n", monthMAX, dayMAX, yearMAX);
